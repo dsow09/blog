7822ba51667c78526900d0fc350080d299f5f663
dsow09 <djibril.sow9717@gmail.com>
2021-12-13 22:39:19 +0000

    updates to deploy heroku

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express');
-const chemin = require('chemin');
+const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/tp-blog'));
 app.get('/*', function(req,res) {
-res.sendFile(chemin.join(__dirname+
-'/dist/tp-blog/index.html'));});
+res.sendFile(path.join(__dirname+'/dist/tp-blog/index.html'));});
 app.listen(process.env.PORT || 8080);
